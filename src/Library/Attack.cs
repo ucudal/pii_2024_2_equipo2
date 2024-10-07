a2fcf3dac6082fbcd79a2aecbee64e2b4d625ccd
Agostina Etchebarren <agostina.etchebarren@correo.ucu.edu.uy>
2024-10-07 08:33:01 -0300

    Create Attack.cs

---
+++ src/Library/Attack.cs
@@ -0,0 +1,14 @@
+﻿namespace Library;
+
+public class Attack
+{
+    public Type AType { get; }
+    public float Damage;
+
+    public Attack(Type aType, float damage)
+    {
+        this.AType = aType;
+        this.Damage = damage;
+    }
+
+}

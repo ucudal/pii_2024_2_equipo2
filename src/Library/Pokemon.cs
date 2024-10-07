0205c75e775543a973a930f23d537a6689b6c31e
Pilar Antelo <pilarantelo.2705@gmail.com>
2024-10-07 08:23:37 -0300

    Create Pokemon.cs

---
+++ src/Library/Pokemon.cs
@@ -0,0 +1,24 @@
+﻿namespace Library;
+
+public class Pokemon
+{
+    private float Life { get; set; }
+    private Type PType { get; set; }
+    private List<Attack> Attacks { get; set; }
+    private int Speed { get; set; }
+
+    public Pokemon(float life, Type pType, List<attack> attacks, int speed)
+    {
+        this.Life = life;
+        this.PType = pType;
+        this.Attack = attacks;
+        this.Speed = speed;
+    }
+
+    public void ReceiveAttack(Attack attack)
+    {
+        baseDamage = attack.damage;
+        this.Life -= AttackEffectivity(attack, this.PType);
+        attack.damage = baseDamage;
+    }
+}

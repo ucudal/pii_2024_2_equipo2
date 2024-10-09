98ca9048265383d36b84bc24e632ca057a52c9ef
LuisVazquezM1 <luis.vazquez@correo.ucu.edu.uy>
2024-10-09 09:48:08 -0300

    ArreglosAttack

--- src/Library/FamilyType/GrassType.cs
+++ src/Library/FamilyType/GrassType.cs
@@ -6,15 +6,15 @@ public class GrassType: IType
 {
     public string Name { get; set; }
 
-    public float CalculateEffectivity(IType oponentType)
+    public float CalculateEffectivity(IType attackType)
     {
-        if (oponentType is WaterType)
-        {
-            return 2.0f;
-        }
-        else if (oponentType is FireType)
+        if (attackType is WaterType)
         {
             return 0.5f;
+        }
+        else if (attackType is FireType)
+        {
+            return 2.0f;
         }
         else
         {
